da828b8639606f8904948b0ce436570926311e82
Erik Summerfield <esummerfield@Eriks-MacBook-Pro.local>
2015-10-30 08:00:02 -0500

    first commit

---
+++ field_css.js
@@ -0,0 +1,52 @@
+/**
+ * @file
+ * Custom JS for enabling/disabling the CodeMirror interface.
+ */
+
+(function ($) {
+  Drupal.behaviors.field_cssCodeMirror = {
+
+    attach: function(context, settings) {
+      // Append enable/disable links.
+      $('.field-widget-field-css-structured-widget .form-textarea-wrapper', context).each(function() {
+        $(this).after(' <a href="#" class="field_css-toggle">Enable syntax highlighting</a>.');
+          var $textarea = $(this).parents('.form-type-textarea').find('textarea');
+          $textarea.val("{\n" + $textarea.val() + "\n}");
+      });
+
+      // Toggle syntax highlighting.
+      $('.field_css-toggle', context).click(function() {
+        var $textarea = $(this).parents('.form-type-textarea').find('textarea');
+        var $textareawrapper = $(this).parents('.form-type-textarea').find('form-textarea-wrapper');
+        var $grippie = $textarea.parents('.resizable-textarea').find('.grippie');
+        var type = $textarea.attr('id').replace('edit-field_css-', '');
+        var mode = 'css';
+        $textareawrapper.addClass("nowrapping");
+
+        // Enable.
+        if (!$(this).hasClass('enabled')) {
+          //$textarea.val("{\n" + $textarea.val() + "\n}");
+          $grippie.hide();
+          var editor = CodeMirror.fromTextArea($textarea.get(0), {
+            mode: mode,
+            tabSize: 0,
+            gutters: ['CodeMirror-linenumbers'],
+            lineNumbers: true
+          });
+          $(this).data('editor', editor);
+          $(this).text(Drupal.t('Disable syntax highlighting')).addClass('enabled');
+        }
+
+        // Disable.
+        else {
+          $(this).data('editor').toTextArea();
+          $grippie.show();
+          $(this).text(Drupal.t('Enable syntax highlighting')).removeClass('enabled');
+          //$textarea.val($textarea.val().replace("\n}", "").replace("{\n",""));
+        }
+        return false;
+      });
+    }
+
+  };
+})(jQuery);
